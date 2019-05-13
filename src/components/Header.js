5c6705f6f7247712f50bea1565028f5fe9baaf27
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-05-13 14:22:09 -0700

    simple dog rendering

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Header() {
+  const header = 'React Stuff!';
+
+  return (
+    <header>
+      <img src="https://i.pinimg.com/736x/a8/d4/59/a8d459c60fd072fee5cf58cba92d8920.jpg"/>
+      <h1>{header}</h1>
+    </header>
+  );
+}
